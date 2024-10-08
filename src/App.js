07f3adc06bc51fe58272c87e286d938dad81266b
Natalia-tonn <natali-tonn@web.de>
2024-10-08 21:38:10 +0200

    add hw 17

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ThemeProvider, createTheme, CssBaseline, Typography, Button } from '@mui/material';
+import styled from '@emotion/styled';
+
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const StyledContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+  padding: '20px',
+}));
+
+const StyledBox = styled('div')(({ theme }) => ({
+  width: '400px',
+  height: '400px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  // // border: `0px solid ${theme.palette.divider}`,
+  // borderRadius: '8px',
+  backgroundColor: theme.palette.background.default,
+  color: theme.palette.text.primary,
+  marginLeft: '-1px', 
+}));
 
 function App() {
+  const [theme, setTheme] = useState(lightTheme);
+
+ 
+  const toggleToLight = () => {
+    setTheme(lightTheme);
+  };
+
+  const toggleToDark = () => {
+    setTheme(darkTheme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <StyledContainer>
+        <StyledBox>
+          <Typography variant="h4">Светлая тема</Typography>
+          <Button variant="contained" onClick={toggleToLight} style={{ marginTop: '20px' }}>
+            Toggle Theme
+          </Button>
+        </StyledBox>
+        <StyledBox>
+          <Typography variant="h4">Темная тема</Typography>
+          <Button variant="contained" onClick={toggleToDark} style={{ marginTop: '20px' }}>
+            Toggle Theme
+          </Button>
+        </StyledBox>
+      </StyledContainer>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
